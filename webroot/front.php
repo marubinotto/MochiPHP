aeb49e05bd8a480ff4de61a02a2e0120a331bb96
Daisuke Morita <daisuke.marubinotto@gmail.com>
2011-05-27 00:47:33 +0900

    [major] Removed "php_value include_path" from .htaccess to be more portable, thus no longer needed to modify it on Windows.

--- webroot/front.php
+++ webroot/front.php
@@ -2,10 +2,18 @@
 // error_reporting(E_ALL | E_STRICT);  // for debug
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
+define('WEBROOT_DIR', dirname(__FILE__));
+
+ini_set('include_path',
+	ini_get('include_path') 
+		. PATH_SEPARATOR . WEBROOT_DIR
+		. PATH_SEPARATOR . WEBROOT_DIR . '/internals/libs'
+);
+
 require_once('mochi/Context.class.php');
 require_once('mochi/FrontController.class.php');
 
-$context = new Context($_REQUEST, $_SERVER, dirname(__FILE__));
+$context = new Context($_REQUEST, $_SERVER, WEBROOT_DIR);
 $controller = new FrontController();
 $controller->processRequest($context);
 ?>
