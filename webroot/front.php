34561dcf98180e59048291903d20ef886981899b
Daisuke Morita <daisuke.marubinotto@gmail.com>
2011-05-27 02:37:26 +0900

    [major] Removed all the php_value entries from .htaccess so that it can be run on hosting servers that don't support for php_value

--- webroot/front.php
+++ webroot/front.php
@@ -4,6 +4,9 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
 $webrootDir = dirname(__FILE__);
 
+ini_set('default_charset', 'UTF-8');
+ini_set('mbstring.internal_encoding', 'UTF-8');	// default encoding for mb_ functions
+
 ini_set('include_path',
 	ini_get('include_path') 
 		. PATH_SEPARATOR . $webrootDir
