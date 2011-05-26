1e772a5e29079d7b24d140746295aff9bfe47c84
Daisuke Morita <daisuke.marubinotto@gmail.com>
2011-05-27 01:00:08 +0900

    Refactoring

--- webroot/front.php
+++ webroot/front.php
@@ -2,18 +2,18 @@
 // error_reporting(E_ALL | E_STRICT);  // for debug
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
-define('WEBROOT_DIR', dirname(__FILE__));
+$webrootDir = dirname(__FILE__);
 
 ini_set('include_path',
 	ini_get('include_path') 
-		. PATH_SEPARATOR . WEBROOT_DIR
-		. PATH_SEPARATOR . WEBROOT_DIR . '/internals/libs'
+		. PATH_SEPARATOR . $webrootDir
+		. PATH_SEPARATOR . $webrootDir . '/internals/libs'
 );
 
 require_once('mochi/Context.class.php');
 require_once('mochi/FrontController.class.php');
 
-$context = new Context($_REQUEST, $_SERVER, WEBROOT_DIR);
+$context = new Context($_REQUEST, $_SERVER, $webrootDir);
 $controller = new FrontController();
 $controller->processRequest($context);
 ?>
